0a65c7cc6892a378c43819089790ac6344ee167e
B9ary01 <bishalrai111@yahoo.com>
2022-08-24 12:58:47 +0100

    add useNavigate to navigate

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from 'react-router';
+
 import Header from "../Header";
+
 const About=()=>{
-   
+    let navigate = useNavigate();
+
+    function handleClick() {
+      navigate('/show')
+    }
     return(
        <>
 
    <h1>About Us</h1>
     <Header/>
-    
+    <button onClick={handleClick}>Go show page</button>
 
     </>
 
